66d2b2eb4ddecc99c66e1f11b4daf4bacc38b8d6
Paul Mejia Mendoza <mejillas_33@hotmail.com>
2020-06-23 20:51:09 +0200

    modifcado app.js para poder dibujar la temp de cualquier ciudad, su descripcion del dia actual y una imagen relacionada con el tiempo actual

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require("express")
 const https = require("https");
+const bodyParser = require("body-parser")
 
 const app = express();
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 
 
 app.get("/", function(req, res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Madrid&appid=eb8f6b02c1e4a49c5d495c77535f82c8&units=metric#"
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/", function(req, res){
+    const city = req.body.cityName
+    const apiKey = "eb8f6b02c1e4a49c5d495c77535f82c8";
+    const unit = "units=metric#"
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&${unit}`
     https.get(url, function(response){
         console.log(response.statusCode);
 
@@ -16,7 +24,7 @@ app.get("/", function(req, res){
             const weatherData = JSON.parse(data)
             const temp = weatherData.main.temp;
             const description = weatherData.weather[0].description;
-            let weatherMadrid = `<h1>The temperature is Madrid is ${temp} degrees celcius today</h1>`
+            let weatherMadrid = `<h1>The temperature is ${city} is ${temp} degrees celcius today</h1>`
             let weatherMadridDescription = `<h1>And the weather is currently ${description}</h1>`
             let icon = weatherData.weather[0].icon
             const imgUrl = `<img src="http://openweathermap.org/img/wn/${icon}@2x.png">`;
@@ -25,7 +33,6 @@ app.get("/", function(req, res){
         })
     })
 })
-
 
 
 app.listen(4000, function(){
